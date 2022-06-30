5020e9901044fa6c3798a3ff604e8b6c358ee73c
Thomas-D40 <alexander.darracq@gmail.com>
2022-06-30 14:58:58 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crm-front';
+  public title!: string;
+
+  public constructor() {
+    this.title = 'crm-front';
+  }
 }
